4e9f3c25db95f4585adff2ce05ff63f25ce47db6
KLOTZ alexandre <klotzalexandre431@gmail.com>
2026-09-01 17:42:45 +0200

    fix: gestion de la division par zero

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 import "fmt"
 func Add(a, b int) int { return a + b }
 func main() {
-fmt.Println("Add(2, 3) =", Add(2, 3))
-Divide(10, 0)
+
+    fmt.Println("Add(2, 3) =", Add(2, 3))
+    Multiply(a, b int) int { return a * b }
+    Divide (10, 0)
 }
-//go run main.go # Sortie: Add(2, 3) = 5
-//git add main.go && git commit -m "feat: fonction Add de base"
-//git push -u origin main
 
-
-
-Divide(a, b int) int { return a / b }
+func Divide(a, b int) (int, error) {
+    if b == 0 { return 0, errors.New("division by zero") }
+    return a / b, nil
+}
