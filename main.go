85998a5b90cdd866e48c53312f1f56c49855558f
Quentin <quentin.guyenot@ynov.com>
2026-09-01 17:07:22 +0200

    feat: exemple de code

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+func Add(a, b int) int { return a + b }
+func main() {
+fmt.Println("Add(2, 3) =", Add(2, 3))
+}
+go run main.go # Sortie: Add(2, 3) = 5
+git add main.go && git commit -m "feat: fonction Add de base"
+git push -u origin main
